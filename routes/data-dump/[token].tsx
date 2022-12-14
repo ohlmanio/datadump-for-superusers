e5d1e6af4b2ee5808bbbabdeab57c2740ea56429
Joshua Ohlman <ohlman@hey.com>
2022-12-14 11:22:19 -0500

    Support uploading CSV

--- routes/data-dump/[token].tsx
+++ routes/data-dump/[token].tsx
@@ -1,6 +1,8 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
 import DateTime from "https://raw.githubusercontent.com/moment/luxon/master/src/datetime.js";
 import { Record, testDB } from "../../backend/database.ts";
+import * as csv from "https://deno.land/x/csv@v0.8.0/mod.ts";
+import { readerFromStreamReader } from "https://deno.land/std/streams/reader_from_stream_reader.ts";
 
 export const handler: Handlers = {
   async POST(req, ctx) {
@@ -10,9 +12,18 @@ export const handler: Handlers = {
 
     const { bucket, user } = tokens[tokens.length - 1];
 
-    const data = await req.json();
+    const contentType = req.headers.get("content-type")?.toLowerCase();
 
-    testDB.put({ bucket, user, event: "put", date: DateTime.now(), size: JSON.stringify(data).length, data })
+    const data = await parseData(req, contentType);
+
+    testDB.put({
+      bucket,
+      user,
+      event: "put",
+      date: DateTime.now(),
+      size: JSON.stringify(data).length,
+      data,
+    });
 
     return new Response("Done");
   },
@@ -23,10 +34,50 @@ export const handler: Handlers = {
 
     const { bucket } = tokens[tokens.length - 1];
 
-    const data = testDB.get({ bucket, event: "put" })
+    const data = testDB.get({ bucket, event: "put" });
     return await ctx.render(data);
   },
 };
+
+async function parseData(req: Request, mimeType = "csv") {
+  if (!req.body) return null;
+
+  const [type, parameters] = mimeType.split(";");
+
+  switch (type.toLowerCase()) {
+    case "application/json":
+      return req.json();
+    case "text/csv":
+    case "application/csv":
+    case "csv":
+    default:
+      if (parameters && parameters.includes("header=present")) {
+        const result = [];
+        for await (
+          const row of csv.readCSVObjects(readerFromStreamReader(req.body.getReader()), {
+            // TODO: encoding from mime type
+            encoding: "utf-8",
+          })
+        ) {
+          result.push(row);
+        }
+
+        return result;
+      } else {
+        const result = [];
+        for await (
+          const row of csv.readCSVRows(readerFromStreamReader(req.body.getReader()), {
+            // TODO: encoding from mime type
+            encoding: "utf-8",
+          })
+        ) {
+          result.push(row);
+        }
+
+        return result;
+      }
+  }
+}
 
 export default function DataList(props: PageProps<Record[]>) {
   const { data } = props;
@@ -34,7 +85,7 @@ export default function DataList(props: PageProps<Record[]>) {
   return (
     <main>
       <h1>{token}</h1>
-      <p>Records: {data.length}</p>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </main>
   );
 }
